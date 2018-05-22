9d5ec55238d11af0527dc3987d5143f9ee8a05e9
Son4ln <son4ln@gmail.com>
2018-05-23 00:03:10 +0700

    update interface

--- js/main.js
+++ js/main.js
@@ -35,4 +35,67 @@ $(document).ready(function(){
             $('#main-nav').removeClass('d-none');
         }
     });
+
+    // km slider
+    var clickWith = 0;
+    var maxSlideWidth = 0;
+    var itemSlider = $('.km-slider .km-slider-inner .km-item');
+    var sliderWidth = '';
+    sliderWidth = $('.km-slider').outerWidth(true);
+    var itemWidth = 0;
+    maxSlideWidth = itemWidth * itemSlider.length - sliderWidth;
+    var translateXval = '';
+    var interval = $('.km-slider').attr('data-interval');
+
+    $('.km-left, .km-right').click(function () {
+        var condition = $(this).hasClass("km-left");
+        if (condition){
+            click(0);
+        } else {
+            click(1);
+        }
+        
+    });
+
+    ResCarouselSize();
+
+    $(window).resize(function () {
+        ResCarouselSize();
+    });
+
+    function ResCarouselSize() {
+        itemSlider.outerWidth(true);
+        sliderWidth = $('.km-slider').outerWidth(true);
+        itemWidth = itemSlider.outerWidth(true);
+        maxSlideWidth = itemWidth * itemSlider.length - sliderWidth;
+    }
+
+    var kmSliderInterval = setInterval(function(){ clickAnimate() }, interval);
+
+    function click(event) {
+        clearInterval(kmSliderInterval);
+        clickAnimate(event);
+    }
+
+    function clickAnimate(event) {
+        if (event === 0) {
+            if (clickWith > 0) {
+                console.log(clickWith);
+                clickWith = parseInt(clickWith) - parseInt(itemWidth);
+                
+                translateXval = parseInt(clickWith);
+            }
+        } else {
+            if (clickWith < maxSlideWidth) {
+                clickWith += itemWidth;
+                translateXval = parseInt(clickWith);
+                if (clickWith > maxSlideWidth) {
+                    translateXval = maxSlideWidth;
+                    clearInterval(kmSliderInterval);
+                }
+            }
+        }
+
+        $('.km-slider .km-slider-inner').css({'transform': `translateX(-${translateXval}px)`});
+    }
   });
